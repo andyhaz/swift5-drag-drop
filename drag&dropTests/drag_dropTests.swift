b06cdb74071b14894eaf664b2285222bb3c839be
andrew hazlett <andyhaz1970@gamile.com>
2021-10-05 18:46:16 -0500

    Initial Commit

---
+++ drag&dropTests/drag_dropTests.swift
@@ -0,0 +1,33 @@
+//
+//  drag_dropTests.swift
+//  drag&dropTests
+//
+//  Created by andyhaz on 10/5/21.
+//
+
+import XCTest
+@testable import drag_drop
+
+class drag_dropTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
